a65468dbcac288f2c6482e846448879ae780b787
Rushan <rushenn123@gmail.com>
2023-02-15 12:09:30 +0400

    Add TOC

--- themes/minio-kes-docs/assets/js/index.js
+++ themes/minio-kes-docs/assets/js/index.js
@@ -1,6 +1,7 @@
 import { header } from "./header";
 import { siteSearch } from "./search";
 import { sidebar } from "./sidebar";
+import { toc } from "./toc";
 
 // Search
 siteSearch();
@@ -10,3 +11,6 @@ header();
 
 // Sidebar
 sidebar();
+
+// TOC
+toc();
